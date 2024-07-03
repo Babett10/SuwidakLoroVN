049ace42859d4aade33b77f4f7b7699777e55f7f
Albert Radja Sihite <113775702+Babett10@users.noreply.github.com>
2024-07-03 11:14:09 +0700

    Git Update

--- Assets/Scripts/Controllers/BottomBarController.cs
+++ Assets/Scripts/Controllers/BottomBarController.cs
@@ -7,6 +7,8 @@ public class BottomBarController : MonoBehaviour
 {
     public TextMeshProUGUI barText;
     public TextMeshProUGUI personNameText;
+    public GameObject NamePanel;
+    public AudioSource voicePlayer;
 
     private int sentenceIndex = -1;
     private StoryScene currentScene;
@@ -14,14 +16,31 @@ public class BottomBarController : MonoBehaviour
     private Animator animator;
     private bool isHidden = false;
 
+    public Dictionary<Speaker, SpriteController> sprites;
+    public GameObject spritesPrefab;
+
+    private Coroutine typingCoroutine;
+    private float speedFactor = 1f;
+
     private enum State
     {
-        PLAYING, COMPLETED
+        PLAYING, SPEEDED_UP, COMPLETED
     }
 
     private void Start()
     {
+        sprites = new Dictionary<Speaker, SpriteController>();
         animator = GetComponent<Animator>();
+    }
+
+    public int GetSentenceIndex()
+    {
+        return sentenceIndex;
+    }
+
+    public void SetSentenceIndex(int sentenceIndex)
+    {
+        this.sentenceIndex = sentenceIndex;
     }
 
     public void Hide()
@@ -42,30 +61,83 @@ public class BottomBarController : MonoBehaviour
     public void ClearText()
     {
         barText.text = "";
+        personNameText.text = "";
     }
 
-    public void PlayScene(StoryScene scene)
+    public void PlayScene(StoryScene scene, int sentenceIndex = -1, bool isAnimated = true)
     {
         currentScene = scene;
-        sentenceIndex = -1;
-        PlayNextSentence();
+        this.sentenceIndex = sentenceIndex;
+        PlayNextSentence(isAnimated);
     }
 
-    public void PlayNextSentence()
+    public void PlayNextSentence(bool isAnimated = true)
     {
-        StartCoroutine(TypeText(currentScene.sentences[++sentenceIndex].text));
-        personNameText.text = currentScene.sentences[sentenceIndex].speaker.speakerName;
-        personNameText.color = currentScene.sentences[sentenceIndex].speaker.textColor;
+        sentenceIndex++;
+        PlaySentence(isAnimated);
+    }
+
+    public void GoBack()
+    {
+        sentenceIndex--;
+        StopTyping();
+        HideSprites();
+        PlaySentence(false);
     }
 
     public bool IsCompleted()
     {
-        return state == State.COMPLETED;
+        return state == State.COMPLETED || state == State.SPEEDED_UP;
     }
 
     public bool IsLastSentence()
     {
         return sentenceIndex + 1 == currentScene.sentences.Count;
+    }
+
+    public bool IsFirstSentence()
+    {
+        return sentenceIndex == 0;
+    }
+
+    public void SpeedUp()
+    {
+        state = State.SPEEDED_UP;
+        speedFactor = 0.25f;
+    }
+
+    public void StopTyping()
+    {
+        state = State.COMPLETED;
+        StopCoroutine(typingCoroutine);
+    }
+
+    public void HideSprites()
+    {
+        while(spritesPrefab.transform.childCount > 0)
+        {
+            DestroyImmediate(spritesPrefab.transform.GetChild(0).gameObject);
+        }
+        sprites.Clear();
+    }
+
+    private void PlaySentence(bool isAnimated = true)
+    {
+        StoryScene.Sentence sentence = currentScene.sentences[sentenceIndex];
+        speedFactor = 1f;
+        typingCoroutine = StartCoroutine(TypeText(sentence.text));
+        personNameText.text = sentence.speaker.speakerName;
+        personNameText.color = sentence.speaker.textColor;
+        if (sentence.audio)
+        {
+            voicePlayer.clip = sentence.audio;
+            voicePlayer.Play();
+        }
+        else
+        {
+            voicePlayer.Stop();
+        }
+        ActSpeakers(isAnimated);
     }
 
     private IEnumerator TypeText(string text)
@@ -77,12 +149,50 @@ public class BottomBarController : MonoBehaviour
         while (state != State.COMPLETED)
         {
             barText.text += text[wordIndex];
-            yield return new WaitForSeconds(0.05f);
+            yield return new WaitForSeconds(speedFactor * 0.05f);
             if(++wordIndex == text.Length)
             {
                 state = State.COMPLETED;
                 break;
             }
         }
+    }
+
+    private void ActSpeakers(bool isAnimated = true)
+    {
+        List<StoryScene.Sentence.Action> actions = currentScene.sentences[sentenceIndex].actions;
+        for(int i = 0; i < actions.Count; i++)
+        {
+            ActSpeaker(actions[i], isAnimated);
+        }
+    }
+
+    private void ActSpeaker(StoryScene.Sentence.Action action, bool isAnimated = true)
+    {
+        SpriteController controller;
+        if (!sprites.ContainsKey(action.speaker))
+        {
+            controller = Instantiate(action.speaker.prefab.gameObject, spritesPrefab.transform)
+                .GetComponent<SpriteController>();
+            sprites.Add(action.speaker, controller);
+        }
+        else
+        {
+            controller = sprites[action.speaker];
+        }
+        switch (action.actionType)
+        {
+            case StoryScene.Sentence.Action.Type.APPEAR:
+                controller.Setup(action.speaker.sprites[action.spriteIndex]);
+                controller.Show(action.coords, isAnimated);
+                return;
+            case StoryScene.Sentence.Action.Type.MOVE:
+                controller.Move(action.coords, action.moveSpeed, isAnimated);
+                break;
+            case StoryScene.Sentence.Action.Type.DISAPPEAR:
+                controller.Hide(isAnimated);
+                break;
+        }
+        controller.SwitchSprite(action.speaker.sprites[action.spriteIndex], isAnimated);
     }
 }
--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -1,69 +1,156 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI; // Pastikan untuk menambahkan ini
 
 public class GameController : MonoBehaviour
 {
     public GameScene currentScene;
     public BottomBarController bottomBar;
-    public BackgroundController backgroundController;
+    public SpriteSwitcher backgroundController;
     public ChooseController chooseController;
+    public QuizController quizController; // Tambahkan ini
+    public AudioController audioController;
+
+    public DataHolder data;
+
+    public string menuScene;
+    public GameObject endingPanel; // Tambahkan ini
 
     private State state = State.IDLE;
 
+    private List<StoryScene> history = new List<StoryScene>();
+
     private enum State
     {
-        IDLE, ANIMATE, CHOOSE
+        IDLE, ANIMATE, CHOOSE, QUIZ // Tambahkan QUIZ state
     }
 
     void Start()
     {
+        if (SaveManager.IsGameSaved())
+        {
+            SaveData data = SaveManager.LoadGame();
+            data.prevScenes.ForEach(scene =>
+            {
+                history.Add(this.data.scenes[scene] as StoryScene);
+            });
+            currentScene = history[history.Count - 1];
+            history.RemoveAt(history.Count - 1);
+            bottomBar.SetSentenceIndex(data.sentence - 1);
+        }
         if (currentScene is StoryScene)
         {
             StoryScene storyScene = currentScene as StoryScene;
-            bottomBar.PlayScene(storyScene);
+            history.Add(storyScene);
+            bottomBar.PlayScene(storyScene, bottomBar.GetSentenceIndex());
             backgroundController.SetImage(storyScene.background);
+            PlayAudio(storyScene.sentences[bottomBar.GetSentenceIndex()]);
         }
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
-        {
-            if (state == State.IDLE && bottomBar.IsCompleted())
+        if (state == State.IDLE) {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
             {
-                if (bottomBar.IsLastSentence())
+                if (bottomBar.IsCompleted())
                 {
-                    PlayScene((currentScene as StoryScene).nextScene);
+                    bottomBar.StopTyping();
+                    if (bottomBar.IsLastSentence())
+                    {
+                        PlayScene((currentScene as StoryScene).nextScene);
+                    }
+                    else
+                    {
+                        bottomBar.PlayNextSentence();
+                        PlayAudio((currentScene as StoryScene)
+                            .sentences[bottomBar.GetSentenceIndex()]);
+                    }
                 }
                 else
                 {
-                    bottomBar.PlayNextSentence();
+                    bottomBar.SpeedUp();
                 }
+            }
+            if (Input.GetMouseButtonDown(1))
+            {
+                if (bottomBar.IsFirstSentence())
+                {
+                    if(history.Count > 1)
+                    {
+                        bottomBar.StopTyping();
+                        bottomBar.HideSprites();
+                        history.RemoveAt(history.Count - 1);
+                        StoryScene scene = history[history.Count - 1];
+                        history.RemoveAt(history.Count - 1);
+                        PlayScene(scene, scene.sentences.Count - 2, false);
+                    }
+                }
+                else
+                {
+                    bottomBar.GoBack();
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                List<int> historyIndicies = new List<int>();
+                history.ForEach(scene =>
+                {
+                    historyIndicies.Add(this.data.scenes.IndexOf(scene));
+                });
+                SaveData data = new SaveData
+                {
+                    sentence = bottomBar.GetSentenceIndex(),
+                    prevScenes = historyIndicies
+                };
+                SaveManager.SaveGame(data);
+                SceneManager.LoadScene(menuScene);
             }
         }
     }
 
-    public void PlayScene(GameScene scene)
+    public void PlayScene(GameScene scene, int sentenceIndex = -1, bool isAnimated = true)
     {
-        StartCoroutine(SwitchScene(scene));
+        if (scene == null)
+        {
+            ShowEndingPanel();
+        }
+        else
+        {
+            StartCoroutine(SwitchScene(scene, sentenceIndex, isAnimated));
+        }
     }
 
-    private IEnumerator SwitchScene(GameScene scene)
+    private IEnumerator SwitchScene(GameScene scene, int sentenceIndex = -1, bool isAnimated = true)
     {
         state = State.ANIMATE;
         currentScene = scene;
-        bottomBar.Hide();
-        yield return new WaitForSeconds(1f);
+        if (isAnimated)
+        {
+            bottomBar.Hide();
+            yield return new WaitForSeconds(1f);
+        }
         if (scene is StoryScene)
         {
             StoryScene storyScene = scene as StoryScene;
-            backgroundController.SwitchImage(storyScene.background);
-            yield return new WaitForSeconds(1f);
-            bottomBar.ClearText();
-            bottomBar.Show();
-            yield return new WaitForSeconds(1f);
-            bottomBar.PlayScene(storyScene);
+            history.Add(storyScene);
+            PlayAudio(storyScene.sentences[sentenceIndex + 1]);
+            if (isAnimated)
+            {
+                backgroundController.SwitchImage(storyScene.background);
+                yield return new WaitForSeconds(1f);
+                bottomBar.ClearText();
+                bottomBar.Show();
+                yield return new WaitForSeconds(1f);
+            }
+            else
+            {
+                backgroundController.SetImage(storyScene.background);
+                bottomBar.ClearText();
+            }
+            bottomBar.PlayScene(storyScene, sentenceIndex, isAnimated);
             state = State.IDLE;
         }
         else if (scene is ChooseScene)
@@ -71,5 +158,25 @@ public class GameController : MonoBehaviour
             state = State.CHOOSE;
             chooseController.SetupChoose(scene as ChooseScene);
         }
+        else if (scene is QuizScene) // Tambahkan ini
+        {
+            state = State.QUIZ;
+            quizController.SetupQuiz(scene as QuizScene);
+        }
+    }
+
+    private void PlayAudio(StoryScene.Sentence sentence)
+    {
+        audioController.PlayAudio(sentence.music, sentence.sound);
+    }
+
+    private void ShowEndingPanel()
+    {
+        endingPanel.SetActive(true);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Menu");
     }
 }
