289177e925566667fc8f9d2568e6e8beaa66042e
Albert Radja Sihite <113775702+Babett10@users.noreply.github.com>
2024-07-16 09:39:48 +0700

    repush

--- Assets/Scripts/Entities/PopUpScene.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewPopUpScene", menuName = "Data/New PopUp Scene")]
-[System.Serializable]
-public class PopUpScene : GameScene
-{
-    public Sprite popupImage;
-    public GameScene nextSceneAfterPopup;
-}
--- Assets/Scripts/Entities/QuizQuestion.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[System.Serializable]
-public class QuizQuestion
-{
-    public string question;
-    public string[] answers;
-    public int correctAnswerIndex;
-}
